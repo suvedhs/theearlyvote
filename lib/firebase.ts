c00a7da606f74eb6f4e3e5e2cbd8f4653b537309
suvedhs <suvedhs@gmail.com>
2022-10-25 12:54:16 -0500

    added firebase auth

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,10 +1,30 @@
 import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
-const postsDirectory = path.join(process.cwd(), 'posts');
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCE4rlIz24mYuzMjvC0EJGtSo9PrZbBMTw",
+  authDomain: "theearlyvote-9cd50.firebaseapp.com",
+  projectId: "theearlyvote-9cd50",
+  storageBucket: "theearlyvote-9cd50.appspot.com",
+  messagingSenderId: "283709702046",
+  appId: "1:283709702046:web:1135475765a756bfe6e08f",
+  measurementId: "G-SEPCBS60M5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export async function getElectionData() {
-  const res = await fetch('potato');
-  return res.json();
+  const res = await fetch('https://api.publicapis.org/entries');
+  return res;
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link'
 import { GetStaticProps } from 'next'
 
 export const getStaticProps: GetStaticProps = async () => {
-  const allEarlyVotingData = getElectionData()
+  const allEarlyVotingData = JSON.stringify(getElectionData())
   return {
     props: {
       allEarlyVotingData,
@@ -16,12 +16,6 @@ export const getStaticProps: GetStaticProps = async () => {
 
 export default function Home({
   allEarlyVotingData
-}: {
-  allEarlyVotingData: {
-    date: string
-    title: string
-    id: string
-  }[]
 }) {
   return (
     <Layout home>
@@ -29,23 +23,10 @@ export default function Home({
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        <p>Live in Texas? Enter your address and find your nearest polling locations.</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allEarlyVotingData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-            </li>
-          ))}
-        </ul>
       </section>
     </Layout>
   )
