841041d48cbe18f6aed824a6940194481d13b03a
suvedhs <suvedhs@gmail.com>
2022-10-25 16:07:54 -0500

    removed firestore. very unnecessary

--- next.config.js
+++ next.config.js
@@ -6,17 +6,6 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const nextConfig = {
-  env: {
-    FIREBASE_CONFIG: {
-      apiKey: "AIzaSyCE4rlIz24mYuzMjvC0EJGtSo9PrZbBMTw",
-      authDomain: "theearlyvote-9cd50.firebaseapp.com",
-      projectId: "theearlyvote-9cd50",
-      storageBucket: "theearlyvote-9cd50.appspot.com",
-      messagingSenderId: "283709702046",
-      appId: "1:283709702046:web:1135475765a756bfe6e08f",
-      measurementId: "G-SEPCBS60M5"
-    }
-  }
 }
 
 module.exports = nextConfig
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import { getElectionData } from '../lib/firebase'
-import Link from 'next/link'
 import { GetStaticProps } from 'next'
 
 export const getStaticProps: GetStaticProps = async () => {
-  const allEarlyVotingData = JSON.stringify(getElectionData())
+  const allEarlyVotingData = {}
   return {
     props: {
       allEarlyVotingData,
