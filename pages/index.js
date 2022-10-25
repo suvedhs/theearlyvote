78dfd07558c4d08b4bf66f82b88986fe89920583
suvedhs <suvedhs@gmail.com>
2022-10-25 01:42:13 -0500

    added lib

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
-import Link from 'next/link'
+import { getElectionData } from '../lib/firebase';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allEarlyVotingData = getElectionData();
+  return {
+    props: {
+      allEarlyVotingData,
+    },
+  };
+}
+
+export default function Home({ allEarlyVotingData }) {
   return (
     <Layout home>
       <Head>
@@ -15,6 +24,20 @@ export default function Home() {
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {/* {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))} */}
+        </ul>
       </section>
     </Layout>
   );
