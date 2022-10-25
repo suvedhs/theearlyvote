09e047102be84f5374909192fd35723c4dd25b89
suvedhs <suvedhs@gmail.com>
2022-10-25 01:06:52 -0500

    ts

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Howdy! I am groovy suvy.]</p>
+        <p>Howdy! I am groovy suvy.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
